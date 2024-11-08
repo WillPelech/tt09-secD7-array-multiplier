df5bb98f988b879934c80bd53b56468651716504
WillScooby <wpelech23@gmail.com>
2024-11-08 17:39:34 -0500

    Name Fix of top module

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_array_secD7 secd7(
+  tt_um_array_secD7 user_project(
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
