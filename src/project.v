e38d80beacc5565e992e63be66194aa91bb2a8b0
WillScooby <wpelech23@gmail.com>
2024-11-08 17:32:48 -0500

    Name Fix of top module

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_array_mult_structural(
+module tt_um_array_secD7_array_multiplier(
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
